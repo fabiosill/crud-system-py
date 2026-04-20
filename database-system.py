2c2b581b2bb4838c4a0d21731a3b3a33bd071d73
Fábio Silva <fabio.silva12022001@gmail.com>
2026-04-20 01:05:37 -0300

    feat: add CRUD system complete

--- database-system.py
+++ database-system.py
@@ -7,96 +7,88 @@ conexao = mysql.connector.connect(
 )
 sql = conexao.cursor()
 
+def options():
+    return int(input(
+        "\n===== This is a database CRUD system :) ======\n\n"
+        "1.CREATE | 2.READ | 3.UPDATE | 4.DELETE | 0.FINISH\n"
+    ))
+
+# === CREATE ===
+def optioninsert():
+    return int(input("This is a create section, then what values do you would like to insert"
+        "\n VALUES: 1.NAME | 2.AGE | 3.CELL | 4.EMAIL | 0.RETURN\n"))
+
 def sqlcreate():
-    commandcreate = 'CREATE TABLE IF NOT EXISTS crudpython (sqlid INT AUTO_INCREMENT PRIMARY KEY, sqlname VARCHAR(60), sqlage TINYINT, sqlnum TINYINT, sqlemail VARCHAR(100))'
-    sql.execute(commandcreate)
-    conexao.commit()
-    print("Table was created successfully")
+    dtname = input("Name: \n")
+    dtage = input("Age: \n")
+    dtcell = input("Cellphone: \n")
+    dtemail = input("E-mail: \n")
 
-def sqlupdate():
-    commandupdate = f'INSERT INTO crudpython (sqlname, sqlage, sqlnum, sqlemail) VALUES ("{dtname}", "{dtage}", "{dtnum}", "{dtemail}")'
-    sql.execute(commandupdate)
-    conexao.commit()
-    print("Update was successfully")
+    cmdsqlinsert = f'INSERT INTO crudpython (sqlname, sqlage, sqlcell, sqlemail) VALUES ("{dtname}", "{dtage}", "{dtcell}", "{dtemail}")'
 
+    sql.execute(cmdsqlinsert)
+    conexao.commit()
+    print("Create successfully")
+
+# === READ ===
 def sqlread():
-    commandread = f'SELECT * FROM '
+    commandread = 'SELECT * FROM crudpython'
     sql.execute(commandread)
     resultado = sql.fetchall()
     print("\nTable")
     for linha in resultado:
-      print(f"ID: {linha[0]} | Name: {linha[1]} | Years Old: {linha[2]} | Number: {linha[3]} | Email: {linha[4]}")
-      
+        print(f"ID: {linha[0]} | Name: {linha[1]} | Years Old: {linha[2]} | Cellphone: {linha[3]} | Email: {linha[4]}")
+
+# === UPDATE ===
+def sqlupdate():
+    idupdate = input("ID: ")
+    collumnoupdate = int(input("UPDATE: 1.NAME | 2.AGE | 3.CELL | 4.EMAIL | 0.RETURN \n=>: "))
+    valueupdate = input("New value: ")
+
+    if collumnoupdate == 1:
+        collumnoupdate = "sqlname"
+    elif collumnoupdate == 2:
+        collumnoupdate = "sqlage"
+    elif collumnoupdate == 3:
+        collumnoupdate = "sqlcell"
+    elif collumnoupdate == 4:
+        collumnoupdate = "sqlemail"
+    else:
+        return
+
+    sql.execute(f"UPDATE crudpython SET {collumnoupdate} = '{valueupdate}' WHERE sqlid = {idupdate}")
+    conexao.commit()
+    if sql.rowcount:
+        print("Updated successfully")
+    else:
+        print("ID not found")
+
+# === DELETE ===
 def sqldelete():
-    commanddelete = f'DROP DATABASE crudpython'
-    print("Table was deleted successfully")
-
-def options():
-    return int(input(
-        "\n===== This is a database CRUD system :) ======\n\n"
-        "1.Create | 2.Read | 3.Update | 4.Delete | 0.Finish\n"
-    ))
-
-def optioninsert():
-    return int(input("This is a create section, then what values do you would like to insert" \
-        "\n VALUES: 1.NAME | 2.AGE | 3.NUM | 4.EMAIL | 0.NO ONE"))
-
-def insertname():
-    dtname = input("Insert a new name\n")
-    cmdinsertname = f'INSERT INTO crudpython (sqlname) VALUES ("{dtname}")'
-    sql.execute(cmdinsertname)
+    iddelete = input("ID: ")
+    commanddelete = f'DELETE FROM crudpython WHERE sqlid = {iddelete}'
+    sql.execute(commanddelete)
     conexao.commit()
-    print("Name inserted successfully")
 
-def insertage():
-    dtage = input("Insert a new age\n")
-    cmdinsertage = f'INSERT INTO crudpython (sqlage) VALUES ("{dtage}")'
-    sql.execute(cmdinsertage)
-    conexao.commit()
-    print("Age inserted successfully")
-
-def insertnum():
-    dtnum = input("Insert a new number\n")
-    cmdinsertnum = f'INSERT INTO crudpython (sqlnum) VALUES ("{dtnum}")'
-    sql.execute(cmdinsertnum)
-    conexao.commit()
-    print("Number inserted successfully")
-
-def insertemail():
-    dtemail = input("Insert a new email\n")
-    cmdinsertemail = f'INSERT INTO crudpython (sqlemail) VALUES ("{dtemail}")'
-    sql.execute(cmdinsertemail)
-    conexao.commit()
-    print("Email inserted successfully")
+    if sql.rowcount:
+        print("Deleted successfully")
+    else:
+        print("ID not found")
 
 option = options()
-insert = optioninsert()
 
 while option != 0:
 
     if option == 1:
-        optioninsert()
-
-        if insert == 1:
-            insertname()
-        elif insert == 2:
-            insertage()
-        elif insert == 3:
-            insertnum()
-        elif insert == 4:
-            insertemail()
-        else:
-            option = options()
+        sqlcreate()
+        option = options()
 
     elif option == 2:
         sqlread()
         option = options()
 
     elif option == 3:
-        dtname = input("This is the Name collumn, please enter a new name\n")
-        dtage = int(input("This is the Age collumn, please enter a new age\n"))
-        dtnum = int(input("This is the Number collumn, please enter a new name\n"))
-        dtemail = input("This is the E-mail collumn, please enter a new name\n")
+        sqlupdate()
         option = options()
 
     elif option == 4:
